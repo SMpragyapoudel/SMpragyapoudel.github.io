3fc057dd76af5f86334396314c991119b67ff71d
Pragya <pragya@Pragyas-MacBook-Air.local>
2022-07-02 08:50:27 -0700

    added

--- reviews/script.js
+++ reviews/script.js
@@ -7,7 +7,7 @@ const reviews = [
     img:
       "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg",
     text:
-      "I'm baby meggings twee health goth +1. Bicycle rights tumeric chartreuse before they sold out chambray pop-up. Shaman humblebrag pickled coloring book salvia hoodie, cold-pressed four dollar toast everyday carry",
+      "Hello,I am Susan Smith",
   },
   {
     id: 2,
@@ -16,7 +16,7 @@ const reviews = [
     img:
       "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883409/person-2_np9x5l.jpg",
     text:
-      "Helvetica artisan kinfolk thundercats lumbersexual blue bottle. Disrupt glossier gastropub deep v vice franzen hell of brooklyn twee enamel pin fashion axe.photo booth jean shorts artisan narwhal.",
+      "hello everyone",
   },
   {
     id: 3,
@@ -25,7 +25,7 @@ const reviews = [
     img:
       "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg",
     text:
-      "Sriracha literally flexitarian irony, vape marfa unicorn. Glossier tattooed 8-bit, fixie waistcoat offal activated charcoal slow-carb marfa hell of pabst raclette post-ironic jianbing swag.",
+      "hello,how are you?",
   },
   {
     id: 4,
@@ -34,7 +34,7 @@ const reviews = [
     img:
       "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg",
     text:
-      "Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic. ",
+      "My name is bill anderson.",
   },
 ];
 
@@ -76,3 +76,26 @@ prevBtn.addEventListener("click",function(){
   }
   showPerson(currentItem);
 });
+
+const form = document.querySelector('.contact_form');
+function sendMessage(e){
+  e.preventDefault();
+  
+   const name = document.querySelector('.name'),
+      email = document.querySelector('.email'),
+      msg = document.querySelector('.msg');
+
+      Email.send({
+    SecureToken : "ad85098d-f4c4-43dd-a015-2e29fecbb5c9",
+    To : 'pragyapoudel77@gmail.com',
+    From : email.value,
+    Subject : Contact form,
+    Body : msg.value
+}).then(
+  message => alert(message)
+);
+
+
+}
+form.addEventListener('submit',sendMsg)
+
